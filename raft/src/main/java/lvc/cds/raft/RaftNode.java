5f3307fe0d71730e2df488c3fc0030c6074a7539
cekella3 <cjc007@lvc.edu>
2021-12-16 21:48:35 -0500

    times

--- raft/src/main/java/lvc/cds/raft/RaftNode.java
+++ raft/src/main/java/lvc/cds/raft/RaftNode.java
@@ -508,7 +508,7 @@ public class RaftNode {
                 peersConnected = true;
             }
 
-            heartbeat = 25000 + rand.nextInt(1500);
+            heartbeat = 30000 + rand.nextInt(1500);
             if(System.currentTimeMillis() - start > heartbeat)
             {
                 return NODE_STATE.CANDIDATE;
